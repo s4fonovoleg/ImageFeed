6272b401598f93c995973aa5fd41a5678d5c6a1b
s4fonovoleg <s4fonovoleg@yandex.ru>
2023-05-18 13:35:50 +0300

    исправление  размера изображений

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -6,29 +6,15 @@ extension ImagesListViewController : UITableViewDelegate {
 	}
 	
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		let photoName = photosName[indexPath.row]
-
-		guard let image = UIImage(named: photoName) else {
+		guard let image = UIImage(named: photosName[indexPath.row]) else {
 			return 0
 		}
-
-		let height = image.size.height
-		let width = image.size.width
-		
-		// let currentCell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-		// let cellImageHeight = currentCell.imageView?.image?.size.height
-		
-		return CGFloat(height)
-		
-//		guard let image = UIImage(named: photosName[indexPath.row]) else {
-//			return 0
-//		}
-//		let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-//		let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-//		let imageWidth = image.size.width
-//		let scale = imageViewWidth / imageWidth
-//		let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
-//		return cellHeight
+		let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+		let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+		let imageWidth = image.size.width
+		let scale = imageViewWidth / imageWidth
+		let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+		return cellHeight
 	}
 }
 
